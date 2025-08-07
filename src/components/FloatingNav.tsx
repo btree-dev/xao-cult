cefa64dcbdca3a84c1ba73e5fbce25ee1ab03365
umeraftab1 <37878539+umeraftab1@users.noreply.github.com>
2025-08-07 21:07:46 +0500

    updated buttong on navigation bar

--- src/components/FloatingNav.tsx
+++ src/components/FloatingNav.tsx
@@ -7,16 +7,16 @@ const FloatingNav = () => {
 
   return (
     <div className={styles.floatingNav}>
-      <button onClick={() => router.push('/home')} title="Home">
+      <button onClick={() => router.push('../dashboard')} title="Home">
         <img src="/FloatingNav/home.svg" alt="Home" />
       </button>
-      <button onClick={() => router.push('../swap-token/swap-token')} title="Swap">
+      <button onClick={() => router.push('../dashboard')} title="Swap">
         <img src="/FloatingNav/swap.svg" alt="Swap" />
       </button>
-      <button onClick={() => router.push('/chat')} title="Chat">
+      <button onClick={() => router.push('../dashboard')} title="Chat">
         <img src="/FloatingNav/chat.svg" alt="Chat" />
       </button>
-      <button onClick={() => router.push('/stats')} title="Stats">
+      <button onClick={() => router.push('../swap-token/swap-token')} title="Stats">
         <img src="/FloatingNav/stats.svg" alt="Stats" />
       </button>
     </div>

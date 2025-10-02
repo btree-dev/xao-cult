8ace5f450c69f477c7949ab16064f66d539deaac
imiqraanwar <iqraanwar431@gmail.com>
2025-10-02 18:51:43 +0500

    updated publicInfo

--- src/pages/public-information.tsx
+++ src/pages/public-information.tsx
@@ -103,33 +103,29 @@ export default function PublicInfoPage() {
           <div className={styles.profileContent}>
             
             <div className={styles.profileActionsContainer}>
-              <button 
-                type="button" 
-                className={styles.actionIconButton} 
-                title="Add Identity"
-              >
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                    <circle cx="9" cy="7" r="4"></circle>
-                    <path d="M5 21v-2a4 4 0 0 1 4-4h0a4 4 0 0 1 4 4v2"></path>
-                    <line x1="19" y1="8" x2="19" y2="14"></line>
-                    <line x1="16" y1="11" x2="22" y2="11"></line>
-                  </svg>
+              <Image
+                src="/User_Add.svg"
+                alt="Add Icon"
+                width={24}
+                height={24}
+                className={styles.actionIconButton}
+                
+              />
 
-              </button>
-              <button 
-                type="button" 
-                className={styles.actionIconButton} 
-                onClick={() =>
-                  handleDeleteIdentity(identities, currentIndex, setIdentities, setCurrentIndex)}
-                disabled={identities.length <= 1}
+              <Image
+                src="/Trash_Full.svg"
+                alt="Delete Icon"
+                width={24}
+                height={24}
+                className={styles.actionIconButton}
                 title="Delete Identity"
-              >
-                <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M3 6H5H21" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-                  <path d="M8 6V4C8 3.46957 8.21071 2.96086 8.58579 2.58579C8.96086 2.21071 9.46957 2 10 2H14C14.5304 2 15.0391 2.21071 15.4142 2.58579C15.7893 2.96086 16 3.46957 16 4V6M19 6V20C19 20.5304 18.7893 21.0391 18.4142 21.4142C18.0391 21.7893 17.5304 22 17 22H7C6.46957 22 5.96086 21.7893 5.58579 21.4142C5.21071 21.0391 5 20.5304 5 20V6H19Z" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-                </svg>
-              </button>
+                onClick={() =>
+                  identities.length > 1 &&
+                  handleDeleteIdentity(identities, currentIndex, setIdentities, setCurrentIndex)
+                }
+              />
             </div>
+
             <div className={styles.profileImageSection}>
               <button type="button" className={styles.arrowButton} onClick={() => handlePrev(setCurrentIndex, identities)}>
                 <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
